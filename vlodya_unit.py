60f74b43fc1c8df198faf31da145f7ed55555be2
Сергей Сытник <98377639+bkitw@users.noreply.github.com>
2022-06-18 14:44:38 +0300

    Delete vlodya_unit.py

--- vlodya_unit.py
+++
@@ -1,9 +0,0 @@
-def sleep_in(weekday, vacation):
-    if weekday == False and vacation == False:
-        return True
-    elif weekday == True and vacation == False:
-        return False
-    elif weekday == False and vacation == True:
-        return True
-    elif weekday == True and vacation == True:
-        return True
